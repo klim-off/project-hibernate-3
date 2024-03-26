bc0795fbd8608cffefa31cc24f336e7ca17d5c32
klim-off <investklimov@gmail.com>
2024-03-26 06:11:12 +0300

    03 Commit

--- src/main/java/com/klimov/Main.java
+++ src/main/java/com/klimov/Main.java
@@ -7,6 +7,8 @@ import com.klimov.dao.CountryDAO;
 import com.klimov.domain.City;
 import com.klimov.domain.Country;
 import com.klimov.domain.CountryLanguage;
+import com.klimov.redis.CityCountry;
+import com.klimov.redis.Language;
 import io.lettuce.core.RedisClient;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -16,6 +18,7 @@ import org.hibernate.cfg.Environment;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 
 import static java.util.Objects.nonNull;
 
@@ -52,15 +55,47 @@ private final CountryDAO countryDAO;
         try (Session session = main.sessionFactory.getCurrentSession()) {
             List<City> allCities = new ArrayList<>();
             session.beginTransaction();
+            List<Country> countries = main.countryDAO.getAll();
 
             int totalCount = main.cityDAO.getTotalCount();
             int step = 500;
             for (int i = 0; i < totalCount; i += step) {
                 allCities.addAll(main.cityDAO.getItems(i, step));
             }
+            List<CityCountry> preparedData = main.transformData(allCities);
             session.getTransaction().commit();
             return allCities;
         }
+    }
+
+    private List<CityCountry> transformData(List<City> allCities) {
+        return cities.stream().map(city -> {
+            CityCountry res = new CityCountry();
+            res.setId(city.getId());
+            res.setName(city.getName());
+            res.setPopulation(city.getPopulation());
+            res.setDistrict(city.getDistrict());
+
+            Country country = city.getCountry();
+            res.setAlternativeCountryCode(country.getAlternativeCode());
+            res.setContinent(country.getContinent());
+            res.setCountryCode(country.getCode());
+            res.setCountryName(country.getName());
+            res.setCountryPopulation(country.getPopulation());
+            res.setCountryRegion(country.getRegion());
+            res.setCountrySurfaceArea(country.getSurfaceArea());
+            Set<CountryLanguage> countryLanguages = country.getLanguages();
+            Set<Language> languages = countryLanguages.stream().map(cl -> {
+                Language language = new Language();
+                language.setLanguage(cl.getLanguage());
+                language.setOfficial(cl.getOfficial());
+                language.setPercentage(cl.getPercentage());
+                return language;
+            }).collect(Collectors.toSet());
+            res.setLanguages(languages);
+
+            return res;
+        }).collect(Collectors.toList());
     }
 
     private SessionFactory prepareRelationalDb() {
